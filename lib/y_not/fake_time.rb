2afbc709744dcce20328e3c593ba41001e7cc099
Jeremy Patton <pattonjp@gmail.com>
2014-02-08 15:21:07 -0600

    militarytime

--- lib/y_not/fake_time.rb
+++ lib/y_not/fake_time.rb
@@ -3,45 +3,103 @@ class FakeTime
 
   def initialize hour, minutes, period
     @hour = hour
-    @minutes = minutes 
+    @minutes = minutes
     @period = period.upcase if period
+    # if period == "PM"
+    #   if @hour!=12
+    #     @hour+=12
+    #   end
+    # else
+    #   if @hour==12
+    #     @hour = 0
+    #   end
+    # end
   end
 
   def self.parse str_time
     @str_time = str_time.strip
     time, period = str_time.split(' ')
     hour, minutes = time.split(':')
+
     new hour.to_i, minutes.to_i, period
   end
 
-  def add_minutes num
+  def add_minutes_rm num
     raise Eception "Time only goes forward here...." unless num > 0
     new_minutes = num + minutes
     add_hours =  new_minutes / 60
     addl_minutes = new_minutes % 60
     new_hour = add_hours % 12
-    spins =0 
-    
-    period_crosses = num/60/12 
+    spins =0
+
+    period_crosses = num/60/12
     spins += period_crosses
     if hour != 12 && period_crosses == 0
-      spins = (hour+ new_hour) >= 12 ? 1 : 0 
+      spins = (hour+ new_hour) >= 12 ? 1 : 0
     end
-    
-    if spins.odd? 
+
+    if spins.odd?
       @period = (['AM','PM'] - [@period]).first
     end
     @minutes = addl_minutes
     @hour += new_hour
-    if @hour > 12 
+    if @hour > 12
       @hour -= 12
     end
     self
   end
 
+  # def add_minutes num
+  #   raise Eception "Time only goes forward here...." unless num > 0
+  #   new_minutes = num + minutes
+  #   add_hours =  new_minutes / 60
+  #   addl_minutes = new_minutes % 60
+  #   new_hour = add_hours % 24
+  #   if num == 96600
+  #     puts ""
+  #     puts "hour #{hour}"
+  #     puts "minutes #{minutes}"
+  #   end
+  #   @minutes = addl_minutes
+  #   @hour = @hour + new_hour
+  #   @hour = @hour %24
+  #   if @hour >= 12
+  #     if @hour == 12
+  #       @period = "PM"
+  #     else
+  #       @hour -= 12
+  #       @period = "PM"
+  #     end
+  #   else
+  #     if @hour == 0
+  #       @hour = 12
+  #     end
+  #     @period = "AM"
+  #   end
+  #   if num == 96600
+  #   puts "new_minutes #{new_minutes}"
+  #   puts "ah #{add_hours}"
+  #   puts "addl_minutes #{addl_minutes}"
+  #   puts "nh #{new_hour}"
+  #   puts "hour #{hour}"
+  #   puts "minutes #{minutes}"
+  # end
+
+
+  #   self
+  # end
+
+  def get_period
+  end
+
   def to_s
     pad_minutes ="0"if minutes.to_s.size < 2
     "#{hour}:#{pad_minutes}#{minutes} #{period}"
+  end
+
+  def to_time_only
+    pad_minutes ="0"if minutes.to_s.size < 2
+    "#{hour}:#{pad_minutes}#{minutes}"
   end
 
 end
--- spec/lib/y_not/fake_time_spec.rb
+++ spec/lib/y_not/fake_time_spec.rb
@@ -1,43 +1,43 @@
 require 'spec_helper'
 
-describe FakeTime do 
+describe FakeTime do
 
-  describe 'one offs' do 
+  describe 'one offs' do
     before(:each) do
       @start_time = Time.new(2014,1,1,11,0)
       @start_input = @start_time.strftime("%l:%M %p").strip
       @fake_time = FakeTime.parse @start_input
     end
     # "[H]H:MM {AM|PM}"
-    it 'can parse time ' do 
+    it 'can parse time ' do
       @fake_time.hour.should == @start_time.hour
       @fake_time.minutes.should ==@start_time.min
       @fake_time.period.should == @start_time.strftime('%p')
     end
 
-    it 'should handle no period' do 
+    it 'should handle no period' do
       @fake_time = FakeTime.parse '1:00'
     end
 
-    it 'handles simple case ' do 
+    it 'handles simple case ' do
       validate_time 1
     end
 
-    it 'handles hour crossings ' do 
+    it 'handles hour crossings ' do
       validate_time 61
     end
-   
-    it 'handles 24 hours' do 
+
+    it 'handles 24 hours' do
       validate_time 60 * 24
     end
-    
-    
-    
-  end    
 
-  describe 'bulk test' do 
-    it 'handles all' do 
-      (0..12).each do |hour|
+
+
+  end
+
+  describe 'bulk test' do
+    it 'handles all' do
+      (0..23).each do |hour|
         @start_time = Time.new(2014,1,1,hour,0)
         (1..100000).each do |min|
           @start_input = @start_time.strftime("%l:%M %p").strip
@@ -53,6 +53,7 @@ def validate_time min
       expected = time.strftime("%l:%M %p").strip
       @fake_time.add_minutes min
       result = @fake_time.to_s
+
       result.should eq(expected), "expected #{expected}, got #{result} starting at: #{@start_time.strftime("%l:%M %p").strip} add: #{min} #{min/60}"
     end
 end
