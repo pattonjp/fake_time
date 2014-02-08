a2b3a44aa5685e4d61338f71f4fdeed1cb9f54ad
Jeremy Patton <pattonjp@gmail.com>
2014-02-08 15:29:14 -0600

    set to utc

--- spec/lib/y_not/fake_time_spec.rb
+++ spec/lib/y_not/fake_time_spec.rb
@@ -1,44 +1,44 @@
 require 'spec_helper'
 
-describe FakeTime do 
+describe FakeTime do
 
-  describe 'one offs' do 
+  describe 'one offs' do
     before(:each) do
-      @start_time = Time.new(2014,1,1,11,0)
+      @start_time = Time.utc(2014,1,1,11,0)
       @start_input = @start_time.strftime("%l:%M %p").strip
       @fake_time = FakeTime.parse @start_input
     end
     # "[H]H:MM {AM|PM}"
-    it 'can parse time ' do 
+    it 'can parse time ' do
       @fake_time.hour.should == @start_time.hour
       @fake_time.minutes.should ==@start_time.min
       @fake_time.period.should == @start_time.strftime('%p')
     end
 
-    it 'should handle no period' do 
+    it 'should handle no period' do
       @fake_time = FakeTime.parse '1:00'
     end
 
-    it 'handles simple case ' do 
+    it 'handles simple case ' do
       validate_time 1
     end
 
-    it 'handles hour crossings ' do 
+    it 'handles hour crossings ' do
       validate_time 61
     end
-   
-    it 'handles 24 hours' do 
+
+    it 'handles 24 hours' do
       validate_time 60 * 24
     end
-    
-    
-    
-  end    
 
-  describe 'bulk test' do 
-    it 'handles all' do 
+
+
+  end
+
+  describe 'bulk test' do
+    it 'handles all' do
       (0..12).each do |hour|
-        @start_time = Time.new(2014,1,1,hour,0)
+        @start_time = Time.utc(2014,1,1,hour,0)
         (1..100000).each do |min|
           @start_input = @start_time.strftime("%l:%M %p").strip
           @fake_time = FakeTime.parse @start_input
